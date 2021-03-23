d2d059839e882d769fbd7ba97978fc8496f79e32
AdamOlsson <Olsson.95_@hotmail.com>
2021-03-23 07:52:31 +0100

    TCP Connect with lower timeout

--- scan.c
+++ scan.c
@@ -6,6 +6,8 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <assert.h>
+#include <fcntl.h>
+#include <errno.h>
 
 static struct addrinfo*
 addrinfo_create(char* ip, char* port)
@@ -34,31 +36,72 @@ addrinfo_destroy(struct addrinfo* addr)
 }
 
 static void
-probe()
-{}
+probe(struct addrinfo* addr, char* port)
+{
+    struct timeval timeout;
+    timeout.tv_sec = 10;
+    timeout.tv_usec = 0;
+
+    int sfd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+
+    if (sfd == -1) {
+        return;
+    }
+
+    int flags = 0;        
+    if((flags = fcntl(sfd, F_GETFL, NULL)) < 0) { 
+        fprintf(stderr, "Error getting socket flags.\n"); 
+        exit(0); 
+    }
+    /* Set non-blocking socket */
+    flags |= O_NONBLOCK; 
+    if(fcntl(sfd, F_SETFL, flags) < 0) { 
+        fprintf(stderr, "Error setting socket flags.\n"); 
+        exit(0); 
+    } 
+
+    int res = connect(sfd, addr->ai_addr, addr->ai_addrlen);
+    if (res == -1) {
+        if (errno == EINPROGRESS) {
+            fprintf(stderr, "Trying to connect...\n");
+
+            fd_set setp;
+            FD_ZERO(&setp); 
+            FD_SET(sfd, &setp);
+            res = select(sfd+1, NULL, &setp, NULL, &timeout);
+            if (res < 0 && errno != EINTR) { 
+                fprintf(stderr, "Error connecting %d - %s\n", errno, strerror(errno)); 
+                exit(0); 
+            } else if(res > 0) {
+                /* Connected */
+                fprintf(stderr, "Port %s is open.\n", port);
+            } else {
+                /* Timeout */
+                fprintf(stderr, "Port %s NOT is open.\n", port);
+            }
+        } else {
+            fprintf(stderr, "Error connecting.\n");
+        }
+    }
+
+    close(sfd);
+}
 
 static void
 scan(char* ip, char* port_range[])
 {
-    printf("%s:[%s, %s]\n", ip, port_range[0], port_range[1]);
+    fprintf(stderr, "%s:[%s, %s]\n", ip, port_range[0], port_range[1]);
 
     struct addrinfo* serv_addr = addrinfo_create(ip, port_range[0]);
-
+    /* TODO:
+        Iterate over ports
+        Test on an open port
+        Perform partial TCP connection
+        Make efficient on large amount of ports, parallellize?
+    */
     struct addrinfo* addr;
     for (addr = serv_addr; addr != NULL; addr = addr->ai_next) {
-        int sfd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
-        if (sfd == -1) {
-            continue;
-        }
-
-        if (connect(sfd, addr->ai_addr, addr->ai_addrlen) == 0) {
-            printf("Port %s is open.\n", port_range[0]);
-        } else {
-            printf("Port %s NOT is open.\n", port_range[0]);
-
-        }
-
-        close(sfd);
+        probe(addr, port_range[0]);
     }
 
     addrinfo_destroy(serv_addr);
